9f843629552bd4282f5ea2e478376aa8f5341730
Prajakta-Shelar <prajaktashelar109@gmail.com>
2024-01-17 20:57:15 +0530

    first commit

---
+++ src/app/Components/header-bottom/header-bottom.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-header-bottom',
+  templateUrl: './header-bottom.component.html',
+  styleUrls: ['./header-bottom.component.css']
+})
+export class HeaderBottomComponent {
+  isNavbarCollapsed = true;
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,43 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderTopComponent } from './Components/header-top/header-top.component';
+import { HeaderBottomComponent } from './Components/header-bottom/header-bottom.component';
+import { FooterComponent } from './Components/footer/footer.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatIconModule } from '@angular/material/icon';
+import { MatInputModule } from '@angular/material/input';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatMenuModule } from '@angular/material/menu';
+import { HomeComponent } from './Components/home/home.component';
+import { NgbCarouselModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderTopComponent,
+    HeaderBottomComponent,
+    FooterComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    MatToolbarModule,
+    MatButtonModule,
+    BrowserAnimationsModule,
+    MatIconModule,
+    MatInputModule,
+    MatFormFieldModule,
+    MatMenuModule,
+    NgbCarouselModule,
+    NgbDropdownModule,
+    NgbCollapseModule
   ],
   providers: [],
   bootstrap: [AppComponent]
